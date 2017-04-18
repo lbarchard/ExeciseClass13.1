d5efcf8d203f2484a63237c5f29975a61e0afba5
Les Barchard <lbarchard@lawn-143-215-116-71.lawn.gatech.edu>
2017-04-18 19:42:26 -0400

    got program working.

---
+++ main.js
@@ -0,0 +1,29 @@
+var http = require("http");
+
+var PORT1 = 7000;
+var PORT2 = 7500;
+
+function handleRequest1(request,response) {
+    response.end("<H1>You Rock!!</H1>");    
+}
+
+function handleRequest2(request,response) {
+    response.end("<H1>You do something less than Rock!!</H1>");    
+}
+
+var server1 = http.createServer(handleRequest1);
+var server2 = http.createServer(handleRequest2);
+
+
+
+
+
+
+server1.listen(PORT1, function(){
+    console.log("server listening on http://localhost:" + PORT1)
+})
+
+
+server2.listen(PORT2, function(){
+    console.log("server listening on http://localhost:" + PORT2)
+})
