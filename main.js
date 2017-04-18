abd467828e4b6ae2108fda17c41c5e1320b6f6fd
Les Barchard <lbarchard@lawn-143-215-116-71.lawn.gatech.edu>
2017-04-18 19:50:56 -0400

    added random function

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ var http = require("http");
 var PORT1 = 7000;
 var PORT2 = 7500;
 
+goodResponses = ["You Rock!!", "You are the very best!!", "I wish I was as cool as you!!"];
+badResponses = ["You definitely don't Rock!!", "You are the very worst!!", "I wish I didn't know you!!"];
+
 function handleRequest1(request,response) {
-    response.end("<H1>You Rock!!</H1>");    
+    index = Math.floor(Math.random() * ((goodResponses.length - 1) - 0 + 1)) + 0
+    response.end("<H1>" + goodResponses[index] + "</H1>");    
 }
 
 function handleRequest2(request,response) {
-    response.end("<H1>You do something less than Rock!!</H1>");    
+    index = Math.floor(Math.random() * ((badResponses.length - 1) - 0 + 1)) + 0
+    console.log(index);
+    response.end("<H1>" + badResponses[index] + "</H1>");    
 }
 
 var server1 = http.createServer(handleRequest1);
